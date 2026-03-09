aee06a683c737be79e81e3a516321633b454a335
Riya Sharma <riyasharma4059@gmail.com>
2026-03-09 18:22:29 +0530

    Remove unused useEffect import from Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "../styles/Navbar.css";
 
